f6c34c135399f289cd012c79245adc2265e8ca21
--get <Johann.Buys97@gmail.com>
2018-08-16 16:27:59 +0200

    Updated front end parsing, passing in the nodes as JsonValue's now

--- dependencygraph/app/controllers/NodeController.scala
+++ dependencygraph/app/controllers/NodeController.scala
@@ -3,10 +3,7 @@ package controllers
 import graph.{DataExtraction, Node}
 import play.api.mvc._
 import javax.inject._
-import play.filters.headers.SecurityHeadersFilter
-
-import scala.collection.mutable.ArrayBuffer
-import scala.concurrent.Future
+import play.api.libs.json.Json
 
 class NodeController @Inject()(cc : ControllerComponents) extends AbstractController(cc) {
 
@@ -14,13 +11,14 @@ class NodeController @Inject()(cc : ControllerComponents) extends AbstractContro
 	DataExtraction.getAllNodeDependencies()
 
 	def node() = Action { implicit request =>
-		Ok(views.html.node(DataExtraction.allNodes))
+		//Ok(views.html.node(DataExtraction.allNodes)
+		Ok(views.html.node(Json.toJson(DataExtraction.allNodes)))
 	}
 	def nodeDetails(id : String) = Action {
 		Ok(views.html.nodeDetails(DataExtraction.allNodes(id)))
 	}
 
 	def filterNodes(seq : String) = Action { implicit request =>
-		Ok(views.html.node(DataExtraction.allNodes.filter(node => node._2._id.contains(seq))))
+		Ok(views.html.node(Json.toJson(DataExtraction.allNodes.filter(node => node._2._id.contains(seq)))))
 	}
 }
--- dependencygraph/app/graph/Node.scala
+++ dependencygraph/app/graph/Node.scala
@@ -1,30 +1,33 @@
 package graph
 
+import play.api.libs.json.{JsValue, Json, Writes}
+
 import scala.collection.mutable.ArrayBuffer
 
-class Node(name : String)
-{
-	val _id : String = name
-	val _sources : ArrayBuffer[Node] = new ArrayBuffer()
-	val _outputs : ArrayBuffer[Node] = new ArrayBuffer()
-	val _dependencies : ArrayBuffer[(Node, Node)] = new ArrayBuffer()
+class Node(name : String) {
+	val _id: String = name
+	val _sources: ArrayBuffer[Node] = new ArrayBuffer()
+	val _outputs: ArrayBuffer[Node] = new ArrayBuffer()
+	val _dependencies: ArrayBuffer[(Node, Node)] = new ArrayBuffer()
 	var depth = 0
-	def addSource(node : Node) : Unit = {
+
+	def addSource(node: Node): Unit = {
 		this._sources += node
 	}
-	def addOutput(node : Node) : Unit = {
+
+	def addOutput(node: Node): Unit = {
 		this._outputs += node
 	}
 
-	def getSources : ArrayBuffer[Node] = {
+	def getSources: ArrayBuffer[Node] = {
 		this._sources
 	}
 
-	def getOutputs : ArrayBuffer[Node] = {
+	def getOutputs: ArrayBuffer[Node] = {
 		this._outputs
 	}
 
-	def dependantExistWithinNode(id : String) : Boolean = {
+	def dependantExistWithinNode(id: String): Boolean = {
 		this._outputs.foreach(output => {
 			if (output._id == id) {
 				return true
@@ -33,16 +36,29 @@ class Node(name : String)
 		false
 	}
 
-	def addDependency(parentNode : Node, actualNode : Node): Unit = {
+	def addDependency(parentNode: Node, actualNode: Node): Unit = {
 		if (!this._dependencies.contains((parentNode, actualNode)))
 			this._dependencies.append((parentNode, actualNode))
 	}
 
-	def hasDependencies : Boolean = {
+	def hasDependencies: Boolean = {
 		this._dependencies.nonEmpty
 	}
 }
 
 object Node {
 	var p_depArray : ArrayBuffer[(Node, Node)] = new ArrayBuffer()
+
+	implicit val implicitNodeWrites = new Writes[Node] {
+		def writes(node : Node) : JsValue = {
+			val nodeJson = Json.obj(
+				"id" -> node._id.toString,
+				"sources" -> node._sources.map(source => source._id),
+				"outputs" -> node._outputs.map(output => output._id),
+				"dependencies" -> node._dependencies.map(dependency => dependency._2._id)
+			)
+			println(nodeJson + "\n\n\n")
+			nodeJson
+		}
+	}
 }
